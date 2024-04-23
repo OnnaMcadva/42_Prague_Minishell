a1ccafbc4e26462f1391f40b43ad8b0d9234b678
makayun <115617658+makayun@users.noreply.github.com>
2024-04-23 15:07:35 +0200

    Add files via upload

---
+++ src/execute/mns_execute_simple.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mns_execute_simple.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmakagon <mmakagon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/04 10:39:48 by mmakagon          #+#    #+#             */
+/*   Updated: 2024/04/23 14:48:34 by mmakagon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/minishell.h"
+
+/* ?? TODO: use the correct parsed element depending on child ?? */
+char	*mns_exec_path(char **paths, char *cmd)
+{
+	int		i;
+	char	*possible_path;
+	char	*possible_exec;
+
+	i = 0;
+	while (paths[i])
+	{
+		possible_path = ft_strjoin(paths[i], "/");
+		possible_exec = ft_strjoin(possible_path, cmd);
+		free(possible_path);
+		if (access(possible_exec, F_OK | X_OK) == 0)
+			return (possible_exec);
+		free(possible_exec);
+		i++;
+	}
+	return (NULL);
+}
+
+/* Simple execute for cases without pipes and redirections. */
+void	mns_execute_simple(t_parsed parsed, char **paths, char **envp)
+{
+	char	*exec;
+	pid_t	pid;
+
+	exec = mns_exec_path(paths, parsed.command);
+	if (!exec)
+		printf ("minishell: command not found: %s\n", parsed.command);
+	else
+	{
+		pid = fork();
+		if (pid == MNS_ERROR)
+			printf ("fork error");
+		else if (pid == CHILD)
+		{
+			if (execve(exec, parsed.args, envp) == MNS_ERROR)
+			{
+				ft_putendl_fd("minishell: permission denied: ", STDOUT_FILENO);
+				exit (MNS_ERROR);
+			}
+		}
+		else
+			wait(NULL);
+	}
+	free (exec);
+}
---
+++ src/minishell.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmakagon <mmakagon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/02 13:44:58 by mmakagon          #+#    #+#             */
+/*   Updated: 2024/04/23 14:50:41 by mmakagon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+void	mns_main_loop(t_data *data, char **envp)
+{
+	while (mns_init(data) != MNS_ERROR)
+	{
+		data->line = readline(PROMPT);
+		if (!data->line)
+			mns_com_exit(data, 0);
+		if (*data->line)
+		{
+			if (mns_parse(data) == ALL_FINE)
+				mns_execute(data, envp);
+			if (data->tkn_count)
+				add_history(data->line);
+		}
+		mns_free_data(data);
+	}
+}
+
+int	main(int argc, char **argv, char **envp)
+{
+	t_data	data;
+
+	signal (SIGINT, mns_sigint_handler);
+	if (argc == 1)
+		mns_main_loop(&data, envp);
+	else
+		printf
+		("%s %s ... - boooo, try without arguments!\n", argv[0], argv[1]);
+}
